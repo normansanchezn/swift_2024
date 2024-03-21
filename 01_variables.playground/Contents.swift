8dbd5ea776627fc6cc1643be73855008adc22434
Norman Sánchez <normansanchezn@gmail.com>
2024-03-20 22:35:23 -0600

    [🎉] Variables in Swift

---
+++ 01_variables.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+/*
+ 'let' es para constantes, las cuales no pueden cambiar.
+ */
+
+let maximumOfLoginAttempts = 3
+
+/*
+ 'var' es para variables, las cuales pueden cambiar su valor
+ */
+var currentLoginAttempt = 0
+
+currentLoginAttempt = 3
+
+print(currentLoginAttempt)
+
+/*
+ Declaracion multiple en una linea, se pueden declarar varias varaibles o constantes en una misma linea, solo se separan mediante comas.
+ */
+
+var x = 0.0, y = 0.0 , z = 0.0
+
+// No lo recomiendo demasiado por que no es legible del todo pero te ayuda a tener todo organizado.
